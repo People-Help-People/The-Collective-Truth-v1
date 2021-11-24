fe22e6ab6281fe28ea2293a4d5c428a33628d2e2
HarishTeens <arishh2@gmail.com>
2021-11-24 12:20:20 +0530

    implement routes
    - landing
    - register
    - explore

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,32 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
 import './App.css';
-import Landing from './components/Landing';
+
 import Footer from './layout/Footer';
 import Header from './layout/Header';
+
+import Landing from './screens/Landing';
+import Register from "./screens/Register";
+import Explore from "./screens/Explore";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <header className="App-container">
-        <Landing />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/register" exact element={<Register />} />
+            <Route path="/explore" exact element={<Explore />} />
+          </Routes>
+        </Router>
       </header>
       <Footer />
-    </div>
+    </div >
   );
 }
 
---
+++ client/src/screens/Explore.js
@@ -0,0 +1,7 @@
+export default function Explore() {
+    return (
+        <div>
+            <h1>Explore</h1>
+        </div>
+    )
+}
